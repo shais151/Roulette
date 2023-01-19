2db3ff3436746486aad83f54c6001a5f782353de
shais151 <shais151.316@bramptonmanor.org.uk>
2023-01-19 14:55:13 +0000

    add Wheel

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class Outcome:
 	"""
 	Each number has a variety of outcomes i.e. ("Red" 1:1)
@@ -11,14 +13,48 @@ class Outcome:
 	def winAmount(self, amount:float) -> float:
 		return self.odds * amount
 
-	def __eq__(self, other) -> boolean:
+	def __eq__(self, other) -> bool:
 		return self.name == other.name
 
-	def __ne__(self, other) -> boolean:
+	def __ne__(self, other) -> bool:
 		return not self.name == other.name
 
 	def __hash__(self) -> int:
 		return hash(self.name)
 
 	def __str__(self) -> str:
-		return
+		return "{name:s} ({odds:d}:1)".format_map(vars(self))
+	
+	def __repr__(self) -> str:
+		return "{class_:s}({name!r}, {odds!r})".format(
+			class_=type(self).__name__, **vars(self)
+		)
+
+class Bin(frozenset):
+	"""
+	Each bin will have 12-14 Outcomes
+	Frozenset means a unique values, fixed once created
+	Extended (inherits all methods from frozenset e.g. .add())
+	Chapter 6, pages 45-48
+	"""
+	pass
+
+class Wheel:
+	"""
+	Contains 38 bins
+	Responsible for Random Number Generation (RNG)
+	Chapter 7, pages 49-52
+	"""
+	def __init__(self):
+		self.bins = tuple(Bin() for _ in range(38))
+		self.rng = random.Random()
+		self.rng.seed(4)
+
+	def addOutcome(self, number, outcome):
+		self.bins[number] = Bin(self.bins[number] | Bin([outcome]))
+
+	def next(self):
+		return self.rng.randint(0, 37) #TODO
+
+	def get(self, bin) -> Bin:
+		return self.bins[bin]
