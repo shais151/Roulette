2c2794e02b96f06d56a3d445ef6371b4eb06d62a
shais151 <shais151.316@bramptonmanor.org.uk>
2023-01-26 14:54:45 +0000

    BinBuilder class

--- main.py
+++ main.py
@@ -58,3 +58,72 @@ class Wheel:
 
 	def get(self, bin) -> Bin:
 		return self.bins[bin]
+
+class BinBuilder:
+	"""
+	Creates all of the winning Outcomes and adds them to each bin on the wheel
+	Each bin has 12-14 different ways that it can be a winner e.g. 1 can be straight, street, corner...
+	Chapter 8, pages 53-58
+	"""
+	def buildBins(self, wheel):
+		pass
+
+	def straightBets(self):
+		"""
+		Bet on a single number paying at 35:1
+		38 bets / 38 outcomes
+		""" 
+		outcomes = []
+		for n in range(0,38):
+			outcomes.append(n , Outcome(str(n), 35))
+		return outcomes
+  
+	def splitBets(self):
+		"""
+		Adjacent pair of numbers (column or row) paying at 17:1
+		114 bets / 114 outcomes
+		"""
+		outcomes = []
+		return outcomes
+  
+	def streetBets(self):
+		"""
+		3 numbers in a single row paying at 11:1
+		12 possible bets / 36 outcomes
+		"""
+		outcomes = []
+		return outcomes
+
+	def lineBets(self):
+		"""
+		A 6 number block (2 street bets) paying at 5:1
+		11 possible bets
+		"""
+		outcomes = []
+		return outcomes
+
+	def dozenBets(self):
+		"""
+		Each number is member of one of three dozens paying at 2:1
+		3 possible bets
+		"""
+		outcomes = []
+		return outcomes
+	
+	def cornerBets(self):
+		"""
+		A square of 4 numbers paying at 8:1
+		22 possible bets / 88 outcomes
+		"""
+		outcomes = []
+		return outcomes
+
+	def outsideBets(self):
+		"""
+		All other bets e.g. Red/Black, Low/High, Even/Odd
+		"""
+		outcomes = []
+		return outcomes
+
+bb = BinBuilder()
+print(bb.straightBets())
